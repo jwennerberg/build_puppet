cc555689bfbb213a92eb1e2a29476a5e881901dc
Johan Wennerberg <johan.x.wennerberg@ericsson.com>
2015-03-27 12:40:29 +0100

    update wrapper script

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+eispuppet_version="3.7.5-1"
+
 echo -n "Wipe existing puppet client installation? (y/n) " ; read wipe
 if [ $wipe = 'y' ]; then
   rm -rf /opt/puppet /etc/puppet /var/lib/puppet /var/log/puppet /var/run/puppet
@@ -10,6 +12,7 @@ fi
 export ftp_proxy=www-proxy.ericsson.se:8080
 export http_proxy=www-proxy.ericsson.se:8080
 export https_proxy=www-proxy.ericsson.se:8080
+export no_proxy="ericsson.se"
 
 ./bin/build.pl -osver $1
 
@@ -18,4 +21,4 @@ if [ -f /etc/rc.d/init.d/puppet ]; then
 else
   init_s=/etc/init.d/puppet
 fi
-echo DEBUG: tar czvf /proj/sekirepo/puppet/packages/eispuppet-3.7.4-1_`hostname -s`-`date +%Y%m%d%H%M`.tgz /opt/puppet /etc/puppet /var/log/puppet /var/run/puppet /var/lib/puppet $init_s
+echo DEBUG: tar czvf /proj/sekirepo/puppet/packages/eispuppet-${eispuppet_version}_`hostname -s`-`date +%Y%m%d%H%M`.tgz /opt/puppet /etc/puppet /var/log/puppet /var/run/puppet /var/lib/puppet $init_s
